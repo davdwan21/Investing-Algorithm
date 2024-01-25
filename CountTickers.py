37328c9266711a740ea5fd406df71d2f22d566da
davdwan21 <141950513+davdwan21@users.noreply.github.com>
2024-01-24 22:17:13 -0800

    Update CountTickers.py
    huh??

--- CountTickers.py
+++ CountTickers.py
@@ -2,44 +2,60 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from collections import Counter
+import json
 
-def Run():
-    def GetTextFromUrl(url):
-        response = requests.get(url)
-        
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            text_content = soup.get_text()
-            return text_content
-        else:
-            print(f"Error: {response.status_code}")
-            return None
+# creates ticker_dict from json file, a dictionary with all stock tickers as values
+with open('tickers.json', 'r') as ticker_file:
+    data = json.load(ticker_file)
 
-    url_list = []
+target_key = 'ticker'
+ticker_dict = {}
+i = 0
 
-    # use testing.txt for testing
-    # use article_urls.txt for actual runs
-    with open('article_urls.txt', 'r') as file:
-        url_list = file.read().splitlines()
+for k, nested_dict in data.items():
+    if target_key in nested_dict:
+        ticker_dict[i] = nested_dict.get(target_key, None)
+        i += 1
 
-    remove_acronyms = []
-    with open('remove_acronyms.txt', 'r') as file:
-        remove_acronyms = file.read().splitlines()
+with open('tickers.txt', 'r') as f:
+    f.read()
+    
+def GetTextFromUrl(url):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text_content = soup.get_text()
+        return text_content
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 
-    pattern = re.compile(r'\b[A-Z]{2,5}\b')
+url_list = []
 
-    all_tickers = []
+# use testing.txt for testing
+# use article_urls.txt for actual runs
+with open('testing.txt', 'r') as file:
+    url_list = file.read().splitlines()
 
-    for url in url_list:
-        web_text = GetTextFromUrl(url)
-        tickers = re.findall(pattern, web_text)
-        tck = set(tickers)
-        all_tickers.extend(tck)
-        
-    filtered_tickers = [ticker for ticker in all_tickers if ticker not in remove_acronyms]
+remove_acronyms = []
+with open('remove_acronyms.txt', 'r') as file:
+    remove_acronyms = file.read().splitlines()
 
-    ticker_count = Counter(filtered_tickers)
-    sorted_tickers = ticker_count.most_common()
+pattern = re.compile(r'\b[A-Z]{2,5}\b')
 
-    for key, value in sorted_tickers:
-        print(f"{key}: {value}")
+all_tickers = []
+
+for url in url_list:
+    web_text = GetTextFromUrl(url)
+    tickers = re.findall(pattern, web_text)
+    tck = set(tickers)
+    all_tickers.extend(tck)
+    
+filtered_tickers = [ticker for ticker in all_tickers if ticker in ticker_dict.values()]
+
+ticker_count = Counter(filtered_tickers)
+sorted_tickers = ticker_count.most_common()
+
+for key, value in sorted_tickers:
+    print(f"{key}: {value}")
